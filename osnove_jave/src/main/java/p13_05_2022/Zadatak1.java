2960d0264d70a580bfb66fd4d637d05edc6f03da
Marija Jankovic <marija.mara.marice@gmail.com>
2022-05-13 19:58:49 +0200

    Minor typo fix

--- osnove_jave/src/main/java/p13_05_2022/Zadatak1.java
+++ osnove_jave/src/main/java/p13_05_2022/Zadatak1.java
@@ -2,6 +2,7 @@ package p13_05_2022;
 
 public class Zadatak1 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello World");
+        
     }
 }
