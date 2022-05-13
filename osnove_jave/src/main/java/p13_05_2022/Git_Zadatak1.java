685e52f5f6fb1e59266c8a0c2e9a6dabab37a4de
Marija Jankovic <marija.mara.marice@gmail.com>
2022-05-13 20:34:54 +0200

    Predavanja 13.05. Git Zadatak 1

---
+++ osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -0,0 +1,11 @@
+package p13_05_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("git clone url\n" +
+                "git status\n" +
+                "git add naziv_fajla.ekstenzija\n" +
+                "git commit -m \"poruka\"\n" +
+                "git push");
+    }
+}
